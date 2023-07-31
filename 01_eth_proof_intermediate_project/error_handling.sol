b79f76c7a6a29c445f2f541d143c8a9053f2b46a
Beginner0x1 <138056577+Beginner0x1@users.noreply.github.com>
2023-07-31 10:42:57 -0400

    Add files via upload

---
+++ 01_eth_proof_intermediate_project/error_handling.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity = 0.8.18; //solidity version
+
+contract  Exception {
+
+    uint256 public value;
+
+    // Function to set a new value, but only if the new value is greater than the current value
+
+    function setValue(uint256 newValue) public {
+        // Use require() to validate the condition
+        require(newValue > value, "New value must be greater than the current value.");
+
+        value = newValue; 
+    }
+
+    // Function to get the square of a number, but only if the number is positive
+    function getSquare(uint256 number) public pure returns (uint256) {
+        
+        // Use assert() to check an invariant condition (a condition that should never be false)
+        assert(number >= 0);
+
+        return number * number;
+    }
+
+    // Function that always reverts with a custom error message
+    function alwaysRevert() public pure {
+        // Use revert() to explicitly revert the transaction  with a custom error message
+        revert("This function always reverts.");
+    }
+}
